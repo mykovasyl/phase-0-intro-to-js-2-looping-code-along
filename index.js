fe93efa2d2c82cb20fba5479419b65c58d1f6ce2
Mykola Vasyl'yev <mykovasyl@gmail.com>
2021-10-04 15:18:12 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(birthdayNames, eventType) {
+    let birthdayMessages = [];
+    for (let i = 0; i < birthdayNames.length; i++) {
+        birthdayMessages.push(`Thank you, ${birthdayNames[i]}, for the wonderful ${eventType} gift!`); 
+
+    }
+    return birthdayMessages;
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer)
+        integer--;
+    }
+    return integer;
+}
